b1f74bab3eab6aaba995069fda7ee62645f72d8f
LebasoraJohnJefferson <johnjefferson.lebasora@evsu.edu.ph>
2022-10-16 23:17:07 +0800

    adding member count in dashboard

--- src/app/components/dashboard-overview/dashboard-overview.component.ts
+++ src/app/components/dashboard-overview/dashboard-overview.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { DashboardService } from 'src/app/service/dashboard.service';
+import { Subscription } from 'rxjs';
+
 
 @Component({
   selector: 'app-dashboard-overview',
@@ -6,10 +9,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard-overview.component.css']
 })
 export class DashboardOverviewComponent implements OnInit {
+  countMemberStatus:any = {
+    'joined':0,
+    'joining':0
+  }
+  private _countStatus:Subscription = new Subscription()
 
-  constructor() { }
+
+  constructor(
+    private _dashboardService:DashboardService
+  ) {
+    this.getAllMemberCount()
+  }
 
   ngOnInit(): void {
   }
+
+  ngOnDestroy() {
+    this._countStatus.unsubscribe()
+  }
+
+  getAllMemberCount(){
+    this._countStatus = this._dashboardService.getCountOfMemberStatus().subscribe((res)=>{
+      this.countMemberStatus = res
+    },(err)=>{
+      console.log(err)
+    })
+  }
+
+  
 
 }
